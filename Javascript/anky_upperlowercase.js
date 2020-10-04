12dbb7999123f9bbcc17957fb229c3714fbac73f
anky19 <42665221+anky19@users.noreply.github.com>
2020-10-04 12:43:09 +0545

    Create anky_upperlowercase.js

---
+++ Javascript/anky_upperlowercase.js
@@ -0,0 +1,11 @@
+
+let word = "this is uppercase string"
+let words= " This is the Lowercase string"
+toUpperCase(word)
+
+function toUpperCase(word) {
+    let result = word.toUpperCase()
+    let remarks =  words.toLowerCase()
+    console.log(result)
+    console.log(remarks)
+}
